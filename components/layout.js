03a98934b10a38201a59c376e0468109fca1207d
Rich Alossi <alossix@gmail.com>
2020-12-07 12:31:44 +0100

    fix layout issues

--- components/layout.js
+++ components/layout.js
@@ -1,11 +1,12 @@
-import Head from 'next/head'
-import Header from './header'
+import Head from "next/head";
+import Header from "./header";
 
 function Layout({ user, loading = false, children }) {
   return (
     <>
       <Head>
-        <title>Next.js with Auth0</title>
+        <title>Upply: Manage Your Job Applications Like a Pro!</title>
+        <link rel="icon" type="image/png" href="/favicon.png" />
       </Head>
 
       <Header user={user} loading={loading} />
@@ -24,12 +25,12 @@ function Layout({ user, loading = false, children }) {
         body {
           margin: 0;
           color: #333;
-          font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
-            Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+          font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+            Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
         }
       `}</style>
     </>
-  )
+  );
 }
 
-export default Layout
+export default Layout;
